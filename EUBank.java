6298db4b2808fafca09abf49e1d493e246a28ff0
Grigoriy <grigoriykosarev@gmail.com>
2017-03-06 15:37:06 +0200

    first commit

---
+++ EUBank.java
@@ -0,0 +1,8 @@
+package ht_4_1;
+
+public abstract class EUBank extends Bank{
+
+    public EUBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
+}
