df12d902e50ac5f1d4f4a6bb979df84c3b177bbc
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-03-20 01:00:47 +0200

    think ...

--- HT_4/Main.java
+++ HT_4/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         //вы должны создать 6 объектов класса User (2 - USBank, 2 - EUBank, 2 - ChinaBank).
 
-    /*bank1 = new Bank(11, "qweqwe", carr);*/
+    /*Bank bankUS1 = new Bank();*/
 
     }
 
