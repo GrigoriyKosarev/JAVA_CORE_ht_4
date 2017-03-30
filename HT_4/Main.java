709e92d09f3cbb13d749def367f4fbaabab18ddf
GrigoriyKosarev <grigoriykosarev@gmail.com>
2017-03-30 13:28:22 +0300

    1.2

--- HT_4/Main.java
+++ HT_4/Main.java
@@ -1,5 +1,7 @@
 package HT_4;
 
+import java.util.Currency;
+
 public class Main implements BankSystemImpl{
 /*
     Класс Main с методом main, где вы должны создать 6 объектов класса User (2 - USBank, 2 - EUBank, 2 - ChinaBank).
@@ -8,10 +10,15 @@ public class Main implements BankSystemImpl{
     и его релизацию.
 */
     public static void main(String[] args) {
-
         //вы должны создать 6 объектов класса User (2 - USBank, 2 - EUBank, 2 - ChinaBank).
 
-    /*Bank bankUS1 = new Bank();*/
+        Bank EUBankGrigoriy = new EUBank(12, "Ukraine", Bank.Currency.EUR, 2,
+                350.00, 79, 1200);
+
+/*
+        User userGrigoriy = new User(1.0, 399.00, 4, "Company cool",
+                400,
+*/
 
     }
 
